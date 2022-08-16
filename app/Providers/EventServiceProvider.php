3fd649be1aba021749890704aae436ea2559d3b8
luismartio231 <luismartio1315@gmail.com>
2022-08-16 09:33:26 -0500

    actualizacion del carrito

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -21,7 +21,7 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
-
+// hola
         Login::class => [
             "App\Listeners\MergeTheCart"
         ],
